39412842e5cccb68eb8af037f4fd746fff2019d3
Sean McWilliams <smcwilliams@wynright.com>
2024-06-03 16:41:25 -0400

    Initial commit

---
+++ SRPExample/CountriesFormatter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Text;
+
+namespace SRPExample
+{
+    public static class CountriesFormatter
+    {
+        public static string FormatForConsole(Country country)
+        {
+            return $"{country.Name}, Capital: {country.Capital} - Region: {country.Region}";
+        }
+    }
+}
